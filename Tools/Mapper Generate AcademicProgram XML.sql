8b7282fd77a359facc088bbc20542ef285f9fab0
WyattBest <32881391+WyattBest@users.noreply.github.com>
2021-04-19 17:01:19 -0600

    Update Mapper Generate AcademicProgram XML.sql
    Capability to automatically insert new items from ProgramOfStudy into Datatel_academicprogramExtensionBase

--- Tools/Mapper Generate AcademicProgram XML.sql
+++ Tools/Mapper Generate AcademicProgram XML.sql
@@ -4,14 +4,17 @@ USE PowerCampusMapper
 -- If you pass values from Slate in PDC format, like DEGREE/CURRICULUM or similar, you can automatically generate
 -- the XML for recruiterMapping.xml without having to use the GUI tool.
 --
+--Should this script automatically insert new items from ProgramOfStudy into Datatel_academicprogramExtensionBase?
+DECLARE @PopulateDatatel_academicprogramExtensionBase BIT = 0
 
-SELECT distinct --CP.LONG_DESC [CP.LONG_DESC]
+SELECT DISTINCT --CP.LONG_DESC [CP.LONG_DESC]
 	CD.LONG_DESC [CD.LONG_DESC]
-	,CC.LONG_DESC [CC.LONG_DESC]
-	--,CP.CODE_VALUE_KEY [CP.CODE_VALUE_KEY]
-	,CD.CODE_VALUE_KEY [CD.CODE_VALUE_KEY]
-	,CC.CODE_VALUE_KEY [CC.CODE_VALUE_KEY]
+	,CC.LONG_DESC [CC.LONG_DESC] --Can be used to populate Datatel_academicprogramExtensionBase.Datatel_name
+	--,CP.CODE_VALUE_KEY [PROGRAM]
+	,CD.CODE_VALUE_KEY [DEGREE]
+	,CC.CODE_VALUE_KEY [CURRICULUM]
 	--,ProgramOfStudyId
+	,CD.CODE_VALUE_KEY + '/' + CC.CODE_VALUE_KEY [Datatel_abbreviation] --Can be used to populate Datatel_academicprogramExtensionBase.Datatel_abbreviation
 INTO #POS
 FROM Campus6.dbo.ProgramOfStudy POS
 --LEFT JOIN Campus6.dbo.CODE_PROGRAM CP
@@ -20,56 +23,78 @@ LEFT JOIN Campus6.dbo.CODE_DEGREE CD
 	ON CD.DegreeId = POS.Degree
 LEFT JOIN Campus6.dbo.CODE_CURRICULUM CC
 	ON CC.CurriculumId = POS.Curriculum
---WHERE CP.CODE_VALUE <> 'UNDER2'
 
---SELECT *
---FROM #POS
+--Programs likely missing from Datatel_academicprogramExtensionBase
+SELECT *
+FROM #POS
+WHERE Datatel_abbreviation NOT IN (
+		SELECT Datatel_abbreviation
+		FROM Datatel_academicprogramExtensionBase
+		)
+
+IF @PopulateDatatel_academicprogramExtensionBase = 1
+BEGIN
+	INSERT INTO [dbo].[Datatel_academicprogramExtensionBase] (
+		[Datatel_abbreviation]
+		,[Datatel_name]
+		)
+	SELECT POS.Datatel_abbreviation
+		,POS.[CC.LONG_DESC]
+	FROM #POS POS
+	WHERE Datatel_abbreviation NOT IN (
+			SELECT Datatel_abbreviation
+			FROM Datatel_academicprogramExtensionBase
+			)
+END
+
+SELECT *
+FROM #POS
 
 SELECT *
 	,'<row RCCodeValue="' + Datatel_abbreviation + '" RCDesc="' + Datatel_name + '" PCDegreeCodeValue="' + (
-		SELECT [CD.CODE_VALUE_KEY]
+		SELECT [DEGREE]
 		FROM #POS
-		WHERE [CD.CODE_VALUE_KEY] = (
+		WHERE [DEGREE] = (
 				SELECT value
-				FROM string_split(Datatel_abbreviation, '/')
+				FROM string_split(D.Datatel_abbreviation, '/')
 				ORDER BY @@rowcount offset 0 rows FETCH NEXT 1 rows ONLY
 				)
-			AND [CC.CODE_VALUE_KEY] = (
+			AND [CURRICULUM] = (
 				SELECT value
-				FROM string_split(Datatel_abbreviation, '/')
+				FROM string_split(D.Datatel_abbreviation, '/')
 				ORDER BY @@rowcount offset 1 rows FETCH NEXT 1 rows ONLY
 				)
 		) + '" PCDegreeDesc="' + (
 		SELECT [CD.LONG_DESC]
 		FROM #POS
-		WHERE [CD.CODE_VALUE_KEY] = (
+		WHERE [DEGREE] = (
 				SELECT value
-				FROM string_split(Datatel_abbreviation, '/')
+				FROM string_split(D.Datatel_abbreviation, '/')
 				ORDER BY @@rowcount offset 0 rows FETCH NEXT 1 rows ONLY
 				)
-			AND [CC.CODE_VALUE_KEY] = (
+			AND [CURRICULUM] = (
 				SELECT value
-				FROM string_split(Datatel_abbreviation, '/')
+				FROM string_split(D.Datatel_abbreviation, '/')
 				ORDER BY @@rowcount offset 1 rows FETCH NEXT 1 rows ONLY
 				)
 		) + '" PCCurriculumCodeValue="' + (
 		SELECT value
-		FROM string_split(Datatel_abbreviation, '/')
+		FROM string_split(D.Datatel_abbreviation, '/')
 		ORDER BY @@rowcount offset 1 rows FETCH NEXT 1 rows ONLY
 		) + '" PCCurriculumDesc="' + (
 		SELECT [CC.LONG_DESC]
 		FROM #POS
-		WHERE [CD.CODE_VALUE_KEY] = (
+		WHERE [DEGREE] = (
 				SELECT value
-				FROM string_split(Datatel_abbreviation, '/')
+				FROM string_split(D.Datatel_abbreviation, '/')
 				ORDER BY @@rowcount offset 0 rows FETCH NEXT 1 rows ONLY
 				)
-			AND [CC.CODE_VALUE_KEY] = (
+			AND [CURRICULUM] = (
 				SELECT value
-				FROM string_split(Datatel_abbreviation, '/')
+				FROM string_split(D.Datatel_abbreviation, '/')
 				ORDER BY @@rowcount offset 1 rows FETCH NEXT 1 rows ONLY
 				)
 		) + '" />' [xml]
-FROM Datatel_academicprogramExtensionBase
+FROM Datatel_academicprogramExtensionBase D
 
 DROP TABLE #POS
