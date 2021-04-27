WyattBest/PowerSlate

commit 325e2bc1d49fa65164884b758d07d14b5e8c918f

Fix bug with comparing changed fields
compare_* fields were not being cast to their proper datatype, causing equality comparisons to fail. This caused unnecessary uploading to the Active source format.

ps_format.py
@@ -63,9 +63,9 @@ def format_app_generic(app, cfg_fields):
                   if v['type'] == int]
     fields_null.extend(
         ['compare_' + field for field in cfg_fields['fields_string']])
-    fields_null.extend(
+    fields_bool.extend(
         ['compare_' + field for field in cfg_fields['fields_bool']])
-    fields_null.extend(
+    fields_int.extend(
         ['compare_' + field for field in cfg_fields['fields_int']])
 
     # Copy nullable strings from input to output, then fill in nulls

commit ff53e302030599a5e7f7bd6a86ad61a423116447

Bugfix; SQL tools update
Fix regression introduced by 325e2bc1d49fa65164884b758d07d14b5e8c918f

Update SQL tools

Tools/Copy DEGREQ to ProgramOfStudy.sql
@@ -1,0 +1,37 @@
+USE campus6
+
+BEGIN TRAN
+
+INSERT INTO [dbo].[ProgramOfStudy] (
+	[Program]
+	,[Degree]
+	,[Curriculum]
+	)
+SELECT DISTINCT CP.ProgramId
+	,CD.DegreeId
+	,CC.CurriculumId
+FROM DEGREQ D
+INNER JOIN CODE_PROGRAM CP
+	ON CP.CODE_VALUE_KEY = D.PROGRAM
+INNER JOIN CODE_DEGREE CD
+	ON CD.CODE_VALUE_KEY = D.DEGREE
+INNER JOIN CODE_CURRICULUM CC
+	ON CC.CODE_VALUE_KEY = D.CURRICULUM
+WHERE 1 = 1
+	AND MATRIC_YEAR = '2021'
+	AND NOT EXISTS (
+		SELECT *
+		FROM ProgramOfStudy PS2
+		WHERE PS2.Program = CP.ProgramId
+			AND PS2.Degree = CD.DegreeId
+			AND PS2.Curriculum = CC.CurriculumId
+		)
+	--Exclude some degree types as needed
+	-- AND DegreeId NOT IN (
+		-- 5
+		-- ,6
+		-- ,13
+		-- ,16
+		-- )
+
+ROLLBACK TRAN

Tools/GRANT EXEC.sql
@@ -10,6 +10,7 @@ GRANT EXEC ON [custom].[PS_updSMSOptIn] to PowerSlate
 GRANT EXEC ON [custom].[PS_selPFChecklist] to PowerSlate
 GRANT EXEC ON [custom].[PS_insNote] to PowerSlate
 GRANT EXEC ON [custom].[PS_updUserDefined] to PowerSlate
+GRANT EXEC ON [custom].PS_updEducation to PowerSlate
 GRANT SELECT, UPDATE, VIEW DEFINITION ON [USERDEFINEDIND]  to PowerSlate
 
 USE [PowerCampusMapper]

ps_format.py
@@ -63,6 +63,10 @@ def format_app_generic(app, cfg_fields):
                   if v['type'] == int]
     fields_null.extend(
         ['compare_' + field for field in cfg_fields['fields_string']])
+    fields_null.extend(
+        ['compare_' + field for field in cfg_fields['fields_bool']])
+    fields_null.extend(
+        ['compare_' + field for field in cfg_fields['fields_int']])
     fields_bool.extend(
         ['compare_' + field for field in cfg_fields['fields_bool']])
     fields_int.extend(