WyattBest/PowerSlate

commit 5670908bda5e886f5b82cd50fd1961740e7dbdd3

Fix #49
Set or clear ENROLL_SEPARATION. Also set ACADEMIC_FLAG = 'N' of admit decision reversed, which wasn't happening before.

SQL/[custom].[PS_updAcademicAppInfo].sql
@@ -1,7 +1,7 @@
 USE [Campus6]
 GO
 
-/****** Object:  StoredProcedure [custom].[PS_updAcademicAppInfo]    Script Date: 2/18/2021 3:20:41 PM ******/
+/****** Object:  StoredProcedure [custom].[PS_updAcademicAppInfo]    Script Date: 2021-08-11 10:01:39 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,8 +12,8 @@ GO
 -- Author:		Wyatt Best
 -- Create date: 2016-11-17
 -- Description:	Updates Status and Decision code for application from Slate.
---				Sets ACADEMIC_FLAG if needed (an API defect).
---				Sets PRIMARY_FLAG
+--				Sets ACADEMIC_FLAG, PRIMARY_FLAG, ENROLL_SEPARATION, DEPARTMENT, POPULATION, COUNSELOR, EXTRA_CURRICULAR, COLLEGE_ATTEND, APPLICATION_DATE.
+--				Sets ADMIT and MATRIC field groups.
 --
 -- 2016-12-15 Wyatt Best:	Added 'Defer' ProposedDecision type.
 -- 2016-12-28 Wyatt Best:	Changed translation CODE_APPDECISION for Waiver from 'ACCP' to 'WAIV'
@@ -271,9 +271,30 @@ BEGIN
 			AND CURRICULUM = @Curriculum
 			AND APPLICATION_FLAG = 'Y';
 
-	-- Set ACADEMIC_FLAG if needed
+	--Update ACADEMIC_FLAG if needed
+	DECLARE @AcademicFlag NVARCHAR(1) = (
+			SELECT CASE 
+					WHEN EXISTS (
+							SELECT *
+							FROM CODE_APPSTATUS
+							WHERE CODE_VALUE_KEY = @AppStatus
+								AND [STATUS] = 'A'
+								AND CONFIRMED_STATUS = 'Y'
+							)
+						AND EXISTS (
+							SELECT *
+							FROM CODE_APPDECISION
+							WHERE CODE_VALUE_KEY = @AppDecision
+								AND [STATUS] = 'A'
+								AND ACCEPTED_DECISION = 'Y'
+							)
+						THEN 'Y'
+					ELSE 'N'
+					END
+			)
+
 	UPDATE dbo.ACADEMIC
-	SET ACADEMIC_FLAG = 'Y'
+	SET ACADEMIC_FLAG = @AcademicFlag
 	WHERE PEOPLE_CODE_ID = @PCID
 		AND ACADEMIC_YEAR = @Year
 		AND ACADEMIC_TERM = @Term
@@ -283,23 +304,43 @@ BEGIN
 		AND CURRICULUM = @Curriculum
 		AND APPLICATION_FLAG = 'Y'
 		AND (
-			ACADEMIC_FLAG <> 'Y'
+			ACADEMIC_FLAG <> @AcademicFlag
 			OR ACADEMIC_FLAG IS NULL
 			)
-		AND EXISTS (
-			SELECT *
-			FROM CODE_APPSTATUS
-			WHERE CODE_VALUE_KEY = @AppStatus
-				AND [STATUS] = 'A'
-				AND CONFIRMED_STATUS = 'Y'
-			)
-		AND EXISTS (
-			SELECT *
-			FROM CODE_APPDECISION
-			WHERE CODE_VALUE_KEY = @AppDecision
-				AND [STATUS] = 'A'
-				AND ACCEPTED_DECISION = 'Y'
-			)
+
+	--Update ENROLL_SEPARATION if needed
+	DECLARE @pdc NVARCHAR(25) = (@program + '/' + @degree + '/' + @curriculum)
+		,@pdc1 NVARCHAR(25) = (@program + '/' + @degree + '/')
+		,@pdc2 NVARCHAR(25) = (@program + '//')
+		,@pdc3 NVARCHAR(25) = ('//')
+		,@EnrollSeparation NVARCHAR(4) = NULL
+
+	IF @AcademicFlag = 'Y'
+		SET @EnrollSeparation = (
+				SELECT TOP 1 LEFT(SETTING, 4)
+				FROM ABT_SETTINGS
+				WHERE AREA_NAME = 'ACA_RECORDS'
+					AND SECTION_NAME = 'STUDENT_CODING_ENROLLED'
+					AND LABEL_NAME IN (
+						@pdc
+						,@pdc1
+						,@pdc2
+						,@pdc3
+						)
+				ORDER BY LABEL_NAME DESC
+				)
+
+	UPDATE dbo.ACADEMIC
+	SET ENROLL_SEPARATION = @EnrollSeparation
+	WHERE PEOPLE_CODE_ID = @PCID
+		AND ACADEMIC_YEAR = @Year
+		AND ACADEMIC_TERM = @Term
+		AND ACADEMIC_SESSION = @Session
+		AND PROGRAM = @Program
+		AND DEGREE = @Degree
+		AND CURRICULUM = @Curriculum
+		AND APPLICATION_FLAG = 'Y'
+		AND COALESCE(ENROLL_SEPARATION, '') <> COALESCE(@EnrollSeparation, '')
 
 	--Update NONTRAD_PROGRAM if needed
 	UPDATE ACADEMIC
@@ -536,6 +577,3 @@ BEGIN
 
 	COMMIT
 END
-GO
-
-

commit 3f6eef0ac8aedfc3839333a1f9446bfb35430e29

Don't update ENROLL_SEPARATION unless ACADEMIC_FLAG changed
Fixes a nasty bug introduced in 5670908bda5e886f5b82cd50fd1961740e7dbdd3. See also issue #49.

SQL/[custom].[PS_updAcademicAppInfo].sql
@@ -1,7 +1,7 @@
 USE [Campus6]
 GO
 
-/****** Object:  StoredProcedure [custom].[PS_updAcademicAppInfo]    Script Date: 2021-08-11 10:01:39 ******/
+/****** Object:  StoredProcedure [custom].[PS_updAcademicAppInfo]    Script Date: 2021-08-13 11:14:01 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -35,6 +35,7 @@ GO
 -- 2021-04-03 Wyatt Best:	Fix missing PDC filters in some update statements. Could have caused multiple records to be affected if student had multiple applications in same YTS.
 --							Change primary flag logic to be more conservative. Rewrote some statements for efficiency.
 -- 2021-04-05 Wyatt Best:	Added switch to control whether @Population will overwrite existing values. Used by MCNY.
+-- 2021-08-13 Wyatt Best:	Added logic to update ENROLL_SEPARATION.
 -- =============================================
 CREATE PROCEDURE [custom].[PS_updAcademicAppInfo] @PCID NVARCHAR(10)
 	,@Year NVARCHAR(4)
@@ -271,8 +272,26 @@ BEGIN
 			AND CURRICULUM = @Curriculum
 			AND APPLICATION_FLAG = 'Y';
 
-	--Update ACADEMIC_FLAG if needed
-	DECLARE @AcademicFlag NVARCHAR(1) = (
+	--Update ACADEMIC_FLAG and ENROLL_SEPARATION if needed
+	--ENROLL_SEPARATION is only updated if the ACADEMIC_FLAG is toggled, otherwise it's left alone.
+	DECLARE @pdc NVARCHAR(25) = (@program + '/' + @degree + '/' + @curriculum)
+		,@pdc1 NVARCHAR(25) = (@program + '/' + @degree + '/')
+		,@pdc2 NVARCHAR(25) = (@program + '//')
+		,@pdc3 NVARCHAR(25) = ('//')
+	DECLARE @EnrolledCode NVARCHAR(4) = (
+			SELECT TOP 1 LEFT(SETTING, 4)
+			FROM ABT_SETTINGS
+			WHERE AREA_NAME = 'ACA_RECORDS'
+				AND SECTION_NAME = 'STUDENT_CODING_ENROLLED'
+				AND LABEL_NAME IN (
+					@pdc
+					,@pdc1
+					,@pdc2
+					,@pdc3
+					)
+			ORDER BY LABEL_NAME DESC
+			)
+		,@NewAcademicFlag NVARCHAR(1) = (
 			SELECT CASE 
 					WHEN EXISTS (
 							SELECT *
@@ -294,7 +313,20 @@ BEGIN
 			)
 
 	UPDATE dbo.ACADEMIC
-	SET ACADEMIC_FLAG = @AcademicFlag
+	SET ACADEMIC_FLAG = @NewAcademicFlag
+		,ENROLL_SEPARATION = CASE 
+			WHEN ACADEMIC_FLAG = 'Y'
+				AND @NewAcademicFlag = 'N'
+				THEN ''
+			WHEN ACADEMIC_FLAG = 'N'
+				AND @NewAcademicFlag = 'Y'
+				THEN @EnrolledCode
+			ELSE ENROLL_SEPARATION
+			END
+		,REVISION_DATE = @Today
+		,REVISION_TIME = @Now
+		,REVISION_OPID = 'SLATE'
+		,REVISION_TERMINAL = '0001'
 	WHERE PEOPLE_CODE_ID = @PCID
 		AND ACADEMIC_YEAR = @Year
 		AND ACADEMIC_TERM = @Term
@@ -304,43 +336,9 @@ BEGIN
 		AND CURRICULUM = @Curriculum
 		AND APPLICATION_FLAG = 'Y'
 		AND (
-			ACADEMIC_FLAG <> @AcademicFlag
+			ACADEMIC_FLAG <> @NewAcademicFlag
 			OR ACADEMIC_FLAG IS NULL
 			)
-
-	--Update ENROLL_SEPARATION if needed
-	DECLARE @pdc NVARCHAR(25) = (@program + '/' + @degree + '/' + @curriculum)
-		,@pdc1 NVARCHAR(25) = (@program + '/' + @degree + '/')
-		,@pdc2 NVARCHAR(25) = (@program + '//')
-		,@pdc3 NVARCHAR(25) = ('//')
-		,@EnrollSeparation NVARCHAR(4) = NULL
-
-	IF @AcademicFlag = 'Y'
-		SET @EnrollSeparation = (
-				SELECT TOP 1 LEFT(SETTING, 4)
-				FROM ABT_SETTINGS
-				WHERE AREA_NAME = 'ACA_RECORDS'
-					AND SECTION_NAME = 'STUDENT_CODING_ENROLLED'
-					AND LABEL_NAME IN (
-						@pdc
-						,@pdc1
-						,@pdc2
-						,@pdc3
-						)
-				ORDER BY LABEL_NAME DESC
-				)
-
-	UPDATE dbo.ACADEMIC
-	SET ENROLL_SEPARATION = @EnrollSeparation
-	WHERE PEOPLE_CODE_ID = @PCID
-		AND ACADEMIC_YEAR = @Year
-		AND ACADEMIC_TERM = @Term
-		AND ACADEMIC_SESSION = @Session
-		AND PROGRAM = @Program
-		AND DEGREE = @Degree
-		AND CURRICULUM = @Curriculum
-		AND APPLICATION_FLAG = 'Y'
-		AND COALESCE(ENROLL_SEPARATION, '') <> COALESCE(@EnrollSeparation, '')
 
 	--Update NONTRAD_PROGRAM if needed
 	UPDATE ACADEMIC