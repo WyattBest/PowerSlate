WyattBest/PowerSlate

commit 5630f974f34f1ace5a302a5703e12bdfaa0b405a

Close SQL connections on exeception
Transactions would often be left open if PS_updDemographics errored.

sync_http.py
@@ -36,12 +36,16 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             else:
                 message = 'Error: Record not found.'
         except Exception as ex:
+            # Close SQL connections
+            ps_core.de_init()
+            
             # Re-initialize and try one more time before returning an error to the user
             print('Attempting to recover from error:', emit_traceback())
             try:
                 CONFIG = ps_core.init(sys.argv[1])
                 message = ps_core.main_sync(q['pid'][0])
             except Exception:
+                ps_core.de_init()
                 message = emit_traceback()
 
         # Write content as utf-8 data

SQL/[custom].[PS_updDemographics].sql
@@ -20,6 +20,7 @@ GO
 -- 2021-03-07 Wyatt Best:	Added @PrimaryLanguage and @HomeLanguage. Don't UPDATE demographics row unless needed. Make most parameters optional.
 -- 2021-03-16 Wyatt Best:	Added @GovernmentId. For safety, existing GOVERNMENT_ID will not be overwritten with Slate-supplied value.
 -- 2021-04-27 Wyatt Best:	Added TOP 1 to existing people search by GOVERNMENT_ID to avoid subquery error.
+-- 2021-09-01 Wyatt Best:	Named transaction.
 -- =============================================
 CREATE PROCEDURE [custom].[PS_updDemographics] @PCID NVARCHAR(10)
 	,@Opid NVARCHAR(8)
@@ -43,7 +44,7 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	BEGIN TRANSACTION
+	BEGIN TRANSACTION PS_updDemographics
 
 	DECLARE @PersonId INT = dbo.fnGetPersonId(@PCID)
 		,@getdate DATETIME = getdate()
@@ -219,7 +220,7 @@ BEGIN
 		SET GOVERNMENT_ID = @GovernmentId
 		WHERE PEOPLE_CODE_ID = @PCID
 
-	COMMIT
+	COMMIT TRANSACTION PS_updDemographics
 END
 GO
 

sync_ondemand.py
@@ -23,6 +23,9 @@ if __name__ == '__main__':
         except AttributeError:
             current_record = None
 
+        # Close SQL connections
+        ps_core.de_init()
+
         with open(sys.argv[1]) as config_file:
             config = json.load(config_file)
             smtp_config = config['smtp']

commit 097d99e2d5a78cf231c0dfe2da688b9724464d53

Fixing bugs introduced in 5630f97

sync_http.py
@@ -36,17 +36,16 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             else:
                 message = 'Error: Record not found.'
         except Exception as ex:
-            # Close SQL connections
-            ps_core.de_init()
-            
             # Re-initialize and try one more time before returning an error to the user
             print('Attempting to recover from error:', emit_traceback())
             try:
+                ps_core.de_init()
                 CONFIG = ps_core.init(sys.argv[1])
                 message = ps_core.main_sync(q['pid'][0])
             except Exception:
-                ps_core.de_init()
                 message = emit_traceback()
+                ps_core.de_init()
+                CONFIG = ps_core.init(sys.argv[1])
 
         # Write content as utf-8 data
         self.wfile.write(message.encode("utf8"))