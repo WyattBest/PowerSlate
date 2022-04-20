f170a5d324ea1e9a3e2b23ba3dfed67662f6fbe5
bendodge-xwis <32881391+WyattBest@users.noreply.github.com>
2022-04-20 08:51:18 -0600

    FA awards: Remove gross amounts and added total line.

--- SQL/[custom].[PS_selPFAwardsXML].sql
+++ SQL/[custom].[PS_selPFAwardsXML].sql
@@ -1,13 +1,12 @@
 USE [Campus6]
 GO
 
-/****** Object:  StoredProcedure [custom].[PS_selPFAwardsXML]    Script Date: 2022-03-23 16:59:36 ******/
+/****** Object:  StoredProcedure [custom].[PS_selPFAwardsXML]    Script Date: 04/19/2022 10:12:34 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
 
 -- =============================================
 -- Author:		Wyatt Best
@@ -16,6 +15,7 @@ GO
 --				Award data is aggregated and intended for display on Slate dashboards.
 --				The XML structure mimics Slate's own Dictionary subquery export types for compatibility with Liquid looping.
 --
+-- 2022-04-19	Wyatt Best:		Removed gross amounts and added total line.
 -- =============================================
 CREATE PROCEDURE [custom].[PS_selPFAwardsXML]
 	-- Add the parameters for the stored procedure here
@@ -68,12 +68,22 @@ BEGIN
 						,type
 					)
 			FROM (
-				--Gross amounts
+				--Individual awards
 				SELECT *
 					,COALESCE([Summer], 0) + COALESCE([Fall], 0) + COALESCE([Spring], 0) AS Total
 				FROM (
-					SELECT fund_long_name
-						,scheduled_amount [amount]
+					SELECT CASE 
+							WHEN net_disbursement_amount > 0
+								AND net_disbursement_amount <> scheduled_amount
+								THEN fund_long_name + ' (Net)'
+							ELSE fund_long_name
+							END [fund_long_name]
+						,CASE 
+							WHEN net_disbursement_amount > 0
+								AND net_disbursement_amount <> scheduled_amount
+								THEN net_disbursement_amount
+							ELSE scheduled_amount
+							END [amount]
 						,IIF(attend_desc = 'T-Summer', 'Summer', attend_desc) [attend_desc]
 					FROM [VMCNYPF01].[PFaids].[dbo].[student] s
 					INNER JOIN [VMCNYPF01].[PFaids].[dbo].[stu_award_year] say
@@ -98,12 +108,17 @@ BEGIN
 				
 				UNION ALL
 				
-				--Net amounts
+				--Grand total
 				SELECT *
 					,COALESCE([Summer], 0) + COALESCE([Fall], 0) + COALESCE([Spring], 0) AS Total
 				FROM (
-					SELECT fund_long_name + ' (Net)' [fund_long_name]
-						,net_disbursement_amount [amount]
+					SELECT 'Totals' [fund_long_name]
+						,CASE 
+							WHEN net_disbursement_amount > 0
+								AND net_disbursement_amount <> scheduled_amount
+								THEN net_disbursement_amount
+							ELSE scheduled_amount
+							END [amount]
 						,IIF(attend_desc = 'T-Summer', 'Summer', attend_desc) [attend_desc]
 					FROM [VMCNYPF01].[PFaids].[dbo].[student] s
 					INNER JOIN [VMCNYPF01].[PFaids].[dbo].[stu_award_year] say
@@ -121,8 +136,8 @@ BEGIN
 							s.alternate_id = @PCID
 							OR s.student_ssn = @GovID
 							)
-						AND net_disbursement_amount > 0
-						AND net_disbursement_amount <> scheduled_amount
+						--AND net_disbursement_amount > 0
+						--AND net_disbursement_amount <> scheduled_amount
 					) a_raw
 				PIVOT(SUM([amount]) FOR attend_desc IN (
 							[Summer]
@@ -130,7 +145,13 @@ BEGIN
 							,[Spring]
 							)) xx
 				) x
-			ORDER BY fund_long_name
+			--Remove empty lines
+			WHERE [Total] > 0
+			ORDER BY CASE fund_long_name
+					WHEN 'Totals'
+						THEN 'zzzz'
+					ELSE fund_long_name
+					END
 			FOR XML path('row')
 				,type
 			) AS [XML]
@@ -142,5 +163,3 @@ BEGIN
 	WHERE s.alternate_id = @PCID
 		OR s.student_ssn = @GovID
 END
-GO
-
